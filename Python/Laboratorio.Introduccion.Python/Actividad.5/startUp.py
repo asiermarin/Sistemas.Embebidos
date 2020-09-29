745d21980fdf39e260e9dfb2aab0301bbf41d051
Asier <asier.marin@opendeusto.es>
2020-09-30 00:24:17 +0200

    notas

--- Python/Laboratorio.Introduccion.Python/Actividad.5/startUp.py
+++ Python/Laboratorio.Introduccion.Python/Actividad.5/startUp.py
@@ -1,5 +1,8 @@
 from core.calculadora import Calculadora
 
+# NOTA: Abir la actividad desde este directorio ./Actividad.5
+# para ejecutarlo sin fallo de compilacion
+
 class Main():
 
     def main():
